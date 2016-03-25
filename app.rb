6d5038276d833da8ebe1a9a1fb2e9c5537391c18
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-25 08:32:16 -0400

    Incorporated data into views

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require "sinatra"
+require_relative "db/models"
 
 get "/" do
+  @fishes = Fish.all
   erb :index, layout: :main
 end
 
 get "/:fishname" do
-  @fish = params[:fishname]
+  @fish = Fish.find_by(name: params[:fishname])
   erb :show, layout: :main
 end
